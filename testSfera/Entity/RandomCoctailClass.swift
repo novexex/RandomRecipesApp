b863af224a9082b0421ee4577423cc8d7a84bdb7
novexex <104654115+novexex@users.noreply.github.com>
2022-11-28 17:50:48 +0300

    Initial commit

---
+++ testSfera/Entity/RandomCoctailClass.swift
@@ -0,0 +1,8 @@
+//
+//  RandomCoctailClass.swift
+//  testSfera
+//
+//  Created by Artour Ilyasov on 28.11.2022.
+//
+
+import Foundation
---
+++ testSfera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testSfera
+//
+//  Created by Artour Ilyasov on 28.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
